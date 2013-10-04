b2b997a77b9e467a8741a12a4036182f8113c6c8
Doug Arley <sgtrama@gmail.com>
2013-10-04 15:17:36 -0400

    Moved API get, added multiple tab interface

--- Resources/app.js
+++ Resources/app.js
@@ -1,78 +1,51 @@
-Ti.UI.backgroundColor = '#dddddd';
+// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+//var myData = require('data');
 
-var myData = require('data');
-var win = Ti.UI.createWindow();
-var table = Ti.UI.createTableView();
-var tableData = [];
-var json, mounts, collected, i, ii, row, nameLabel, nickLabel, isFlying, wowRemoteResponse, wowRemoteError;
+var myData2 = require('display');
+Titanium.UI.setBackgroundColor('#000');
 
-wowRemoteResponse = function() {
-		Ti.API.debug(this.responseText);
- 
-    	json = JSON.parse(this.responseText);
-		mounts = json.mounts.collected;
-		isFlying = '';
-				
-    	row = Ti.UI.createTableViewRow({
-            height:'60dp'
-        });
-        nameLabel = Ti.UI.createLabel({
-            text: json.name,
-            font: {
-                fontSize:'24dp',
-            	fontWeight:'bold'
-        	},
-        	height:'auto',
-        	left:'10dp',
-        	top:'5dp',
-        	color:'#000',
-        	touchEnabled:false
-        });
-			
-	   for(ii=0;ii<mounts.length;ii++) {
-	   		if(mounts[ii].creatureId == 32153) {
-	   			isFlying = "Has Time-Lost Proto-Drake.";
-			}
-	   }
-        
-        nickLabel = Ti.UI.createLabel({
-        	text: isFlying,
-        	font:{
-            	fontSize:'16dp'
-        	},
-        	height:'auto',
-        	left:'15dp',
-        	bottom:'5dp',
-        	color:'#000',
-        	touchEnabled:false
-        });
- 
-        row.add(nameLabel);
-        row.add(nickLabel);
-        tableData.push(row);
+// create tab group
+var tabGroup = Titanium.UI.createTabGroup();
 
-    	table.setData(tableData);
-};
+// create base UI tab and root window
+var win = Titanium.UI.createWindow({  
+    title: "Main Tab",
+    backgroundColor:'#fff'
+});
 
-wowRemoteError = function(e) {
-	    Ti.API.debug("STATUS: " + this.status);
-	    Ti.API.debug("TEXT:   " + this.responseText);
-	    Ti.API.debug("ERROR:  " + e.error);
-	    alert('There was an error retrieving the remote data. Try again.');
-};
+var tab = Titanium.UI.createTab({  
+    icon:'KS_nav_views.png',
+    title:"Main",
+    window:win
+});
 
-for(i=0;i<myData.data.length;i++){
-	var url = "http://us.battle.net/api/wow/character/" + myData.data[i].charserver + "/" + myData.data[i].charname + "?fields=mounts";
+var label = Titanium.UI.createLabel({
+	color:'#999',
+	text:"Stand-in Main Window",
+	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto'
+});
 
-	var xhr = Ti.Network.createHTTPClient({
-	    onload: wowRemoteResponse,
-	    onerror: wowRemoteError,
-	    timeout:5000
-	});
+var tab2 = Titanium.UI.createTab({  
+    icon:'KS_nav_views.png',
+    title:"Data",
+    window:myData2.display
+});
 
-	xhr.open("GET", url);
-	xhr.send();
-}
- 
-win.add(table);
-win.open();
+/*
+var label2 = Titanium.UI.createLabel({
+	color:'#999',
+	text:"",
+	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto'
+});
+*/
+
+win.add(label);
+tabGroup.addTab(tab);  
+tabGroup.addTab(tab2);
+
+// open tab group
+tabGroup.open();
